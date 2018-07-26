b277ae170994b6b7ce8b82048b46ec844282da6a
Stoney Kang <sikang99@gmail.com>
2018-07-26 11:53:26 +0900

    Modified to use the latest libav library

--- main.go
+++ main.go
@@ -6,31 +6,40 @@ import (
 
 	"flag"
 	"fmt"
+	"log"
 	"net/url"
 )
 
 func main() {
 	var driver, device string
 	var display bool = false
+
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
 	flag.StringVar(&driver, "driver", DRIVER, "set capture driver")
 	flag.StringVar(&device, "device", DEVICE, "set capture device")
 	flag.BoolVar(&display, "display", false, "enable sdl display")
+
 	flag.Parse()
 	if flag.NArg() == 0 {
-		fmt.Println("no thing to mux")
+		log.Println("no thing to mux")
 		flag.Usage()
 		return
 	}
+
 	uri, err := url.Parse(flag.Arg(0))
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+
 	var format string
 	if uri.Scheme == "rtsp" {
 		format = "rtsp"
 	}
+
 	src := media.MediaSource{media.NewVideoSource(driver, device), &(media.AudioSource{})}
+
 	m, err := media.NewMuxer(&src, format, flag.Arg(0))
 	if err == nil {
 		if display {
